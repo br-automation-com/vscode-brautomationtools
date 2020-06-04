1706966d8befd16e1dbb24f69865347aa2d6be51
Patrick Tanner <patrick.tanner@br-automation.com>
2020-06-04 18:57:44 +0200

    Moved command registering to BrCommands.ts

--- src/BRCommands.ts
+++ src/BRCommands.ts
@@ -4,9 +4,46 @@ import * as BRConfiguration from "./BRConfiguration";
 import * as BRDialogs from './BRDialogs';
 import * as BRAsProjectWorkspace from './BRAsProjectWorkspace';
 
+/**
+ * Registers all commands of the extension
+ * @param context Extension context to push disposables
+ */
+export function registerCommands(context: vscode.ExtensionContext) {
+    let disposable: vscode.Disposable | undefined; // disposable to push for clean up on deactivation
+    //#region Commands accessible in UI by using Ctrl+Shift+P
+	// Command: Test
+	disposable = vscode.commands.registerCommand('vscode-brautomationtools.test',
+		(arg1, arg2) => testCommand(arg1, arg2));
+	context.subscriptions.push(disposable);
+	// Command: Force activation of extension
+	disposable = vscode.commands.registerCommand('vscode-brautomationtools.forceActivate',
+		() => {});
+	context.subscriptions.push(disposable);
+	// Command: Update configuration of installed AS versions from file system search
+	disposable = vscode.commands.registerCommand('vscode-brautomationtools.updateConfigInstalledAsVersionsFromSearch',
+		updateConfigInstalledAsVersionsFromSearch);
+	context.subscriptions.push(disposable);
+    //#endregion Commands accessible in UI by using Ctrl+Shift+P
+    
+    //#region Commands for dialogs, so they can be used in tasks...
+	// Dialog command: select project configuration
+	disposable = vscode.commands.registerCommand('vscode-brautomationtools.dialogSelectASProjectConfiguration',
+		BRDialogs.selectASProjectConfiguration);
+	context.subscriptions.push(disposable);
 
-export async function testCommand() {
-    console.log('Selection test started');
+	// Dialog command: select build mode
+	disposable = vscode.commands.registerCommand('vscode-brautomationtools.dialogSelectBuildMode',
+		BRDialogs.selectBuildMode);
+	context.subscriptions.push(disposable);
+	//#endregion Commands for dialogs, so they can be used in tasks...
+}
+
+export async function testCommand(arg1: any, arg2: any) {
+	console.log('Selection test started');
+	console.log('arg1');
+	console.log(arg1);
+	console.log('arg2');
+	console.log(arg2);
     const selected = await BRAsProjectWorkspace.getUserSettings();
     console.log(`output is ${selected}`);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,45 +4,14 @@ import * as BRCommands from './BRCommands';
 import * as BRDialogs from './BRDialogs';
 import * as BrIecHeaderProvider from './BrIecHeaderProvider';
 import * as BrCppToolsApi from './BrCppToolsApi';
+import * as BrAsBuildTaskProvider from './BrAsBuildTaskProvider';
 
 // Activation event
 export async function activate(context: vscode.ExtensionContext) {
-	let disposable; // disposable to push for clean up on deactivation
-
 	vscode.window.showInformationMessage('Extension B&R Automation Tools is now active');
 
-	//#region Register commands
-	// Command: Test
-	disposable = vscode.commands.registerCommand('vscode-brautomationtools.test',
-		BRCommands.testCommand);
-	context.subscriptions.push(disposable);
-
-	// Command: Force activation of extension
-	disposable = vscode.commands.registerCommand('vscode-brautomationtools.forceActivate',
-		() => {});
-	context.subscriptions.push(disposable);
-	
-	// Command: Update configuration of installed AS versions from file system search
-	disposable = vscode.commands.registerCommand('vscode-brautomationtools.updateConfigInstalledAsVersionsFromSearch',
-		BRCommands.updateConfigInstalledAsVersionsFromSearch);
-	context.subscriptions.push(disposable);
-	//#endregion Register commands
-
-	//#region Register commands for dialogs, so they can be used in tasks...
-	// Dialog command: select project configuration
-	disposable = vscode.commands.registerCommand('vscode-brautomationtools.dialogSelectASProjectConfiguration',
-		BRDialogs.selectASProjectConfiguration);
-	context.subscriptions.push(disposable);
-
-	// Dialog command: select build mode
-	disposable = vscode.commands.registerCommand('vscode-brautomationtools.dialogSelectBuildMode',
-		BRDialogs.selectBuildMode);
-	context.subscriptions.push(disposable);
-	//#endregion Register commands for dialogs, so they can be used in tasks...
-
-	//#region External API
+	BRCommands.registerCommands(context);
     BrCppToolsApi.registerCppToolsConfigurationProvider();
-	//#endregion External API
 }
 
 // this method is called when your extension is deactivated
