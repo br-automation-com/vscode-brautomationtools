6ca158186031264dabca628daf8e867c74f899db
Patrick Tanner <a.d.e.suisse@gmail.com>
2023-04-28 06:58:25 +0200

    Configured commands and menus for package update in package.json #61

--- src/ExtensionCommands.ts
+++ src/ExtensionCommands.ts
@@ -6,6 +6,8 @@
 import * as vscode from "vscode";
 import { Environment } from "./Environment/Environment";
 import * as BRDialogs from "./UI/BrDialogs";
+import { logger } from "./Tools/Logger";
+import { AsPackageFile } from "./Workspace/Files/AsPackageFile";
 
 /**
  * Registers all commands of the extension
@@ -43,6 +45,23 @@ function registerContributedCommands(context: vscode.ExtensionContext): void {
         "vscode-brautomationtools.changeActiveConfiguration", //
         changeActiveConfiguration
     );
+    // Update all AS Package entries
+    disposable = vscode.commands.registerCommand(
+        "vscode-brautomationtools.updateAllPackageFileEntries", //
+        (fileUri) => updateAllPackageFileEtriesCommand(fileUri)
+    );
+    context.subscriptions.push(disposable);
+    // Add missing AS Package entries
+    disposable = vscode.commands.registerCommand(
+        "vscode-brautomationtools.addMissingPackageFileEntries", //
+        (fileUri) => addMissingPackageFileEntriesCommand(fileUri)
+    );
+    context.subscriptions.push(disposable);
+    // Remove non existing AS Package entries
+    disposable = vscode.commands.registerCommand(
+        "vscode-brautomationtools.removeNonExistingPackageFileEntries", //
+        (fileUri) => removeNonExistingPackageFileEntriesCommand(fileUri)
+    );
     context.subscriptions.push(disposable);
 }
 
@@ -75,4 +94,62 @@ async function changeActiveConfiguration(): Promise<void> {
     const config = await BRDialogs.selectASProjectConfiguration(project);
     if (config === undefined) return;
     await project.changeActiveConfiguration(config.name);
+}
+
+async function updateAllPackageFileEtriesCommand(fileUri: unknown): Promise<void> {
+    // TODO make also new command "add reference to package" -> select normal file makes file ref, selecting package file makes pkg ref
+    logger.detail("Try to update package file");
+    fileUri = fileUri ?? vscode.window.activeTextEditor?.document.uri;
+    if (!(fileUri instanceof vscode.Uri)) {
+        logger.error("Update package command call first argument is not a URI");
+        return;
+    }
+    // Update package
+    // TODO keep the function here? where would this function fit well?
+    const pkg = await AsPackageFile.createFromFile(fileUri);
+    await pkg?.updateChildren(true);
+    const success = await pkg?.writeToFile();
+    if (success === true) {
+        logger.info(`Package file ${logger.formatUri(fileUri)} updated.`);
+    } else {
+        logger.error(`Failed to update package file ${logger.formatUri(fileUri)}`);
+    }
+}
+
+async function addMissingPackageFileEntriesCommand(fileUri: unknown): Promise<void> {
+    logger.detail("Try to add missing entries to package file");
+    fileUri = fileUri ?? vscode.window.activeTextEditor?.document.uri;
+    if (!(fileUri instanceof vscode.Uri)) {
+        logger.error("Update package command call first argument is not a URI");
+        return;
+    }
+    // Update package
+    // TODO keep the function here? where would this function fit well?
+    const pkg = await AsPackageFile.createFromFile(fileUri);
+    await pkg?.addMissingChildren();
+    const success = await pkg?.writeToFile();
+    if (success === true) {
+        logger.info(`Added missing entries to package file ${logger.formatUri(fileUri)}.`);
+    } else {
+        logger.error(`Failed to update package file ${logger.formatUri(fileUri)}`);
+    }
+}
+
+async function removeNonExistingPackageFileEntriesCommand(fileUri: unknown): Promise<void> {
+    logger.detail("Try to remove not existing entries from package file");
+    fileUri = fileUri ?? vscode.window.activeTextEditor?.document.uri;
+    if (!(fileUri instanceof vscode.Uri)) {
+        logger.error("Update package command call first argument is not a URI");
+        return;
+    }
+    // Update package
+    // TODO keep the function here? where would this function fit well?
+    const pkg = await AsPackageFile.createFromFile(fileUri);
+    await pkg?.removeNonExtistingChildren(true); // TODO from settings or dialog here?
+    const success = await pkg?.writeToFile();
+    if (success === true) {
+        logger.info(`Removed not existing entries from package file ${logger.formatUri(fileUri)}`);
+    } else {
+        logger.error(`Failed to update package file ${logger.formatUri(fileUri)}`);
+    }
 }
